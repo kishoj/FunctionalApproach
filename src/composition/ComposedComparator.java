ab12b4f82211cc0b5edd88ef32f92438548a16c9
Kishoj Bajracharya <kishoj@gmail.com>
2016-11-24 06:34:58 +0700

    [Functional Approach] Added the comparator for list

---
+++ src/composition/ComposedComparator.java
@@ -0,0 +1,24 @@
+package composition;
+
+import java.util.Comparator;
+
+@SuppressWarnings("unchecked")
+public class ComposedComparator<T> implements Comparator<T> {
+	private Comparator<T>[] comparators;
+
+	public ComposedComparator(Comparator<T>... comparators) {
+		this.comparators = comparators;
+	}
+
+	@Override
+	public int compare(T o1, T o2) {
+		int result = 0;
+		for (Comparator<T> comparator : comparators) {
+			result = comparator.compare(o1 , o2);
+			if (result != 0) {
+				return result;
+			}
+		}
+		return result;
+	}
+}
