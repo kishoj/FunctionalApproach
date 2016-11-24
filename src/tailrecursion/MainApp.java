a6e275c56a4690f2fc084029ddad28e4592be195
Kishoj Bajracharya <kishoj@gmail.com>
2016-11-24 07:03:40 +0700

    [Functional Approach] Added the tail recursion

---
+++ src/tailrecursion/MainApp.java
@@ -0,0 +1,14 @@
+package tailrecursion;
+
+import java.math.BigDecimal;
+import java.util.stream.IntStream;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// Fibonacci numbers from 0 to 10
+		IntStream.range(0, 11).mapToObj(BigDecimal::valueOf).forEach(number -> {
+			System.out.println("f(" + number + "):= " + Fibonacci.valueOf(number));
+		});
+	}
+}
